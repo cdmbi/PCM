f2c1d5a3d6f37502b8fde46dc10c90f4a4378021
Likit Preeyanon <preeyano@msu.edu>
2015-03-28 11:01:38 +0700

    added some commands for building rdkit

--- install.py
+++ install.py
@@ -6,8 +6,7 @@ try:
 except ImportError:
     print >> sys.stderr, 'rdkit not installed...'
     print >> sys.stderr, 'build rdkit...'
-    # subprocess.call(['git', 'clone', 'https://github.com/rdkit/conda-rdkit.git'])
-
-os.chdir('conda-rdkit')
-subprocess.call(['conda', 'build', 'boost'])
-subprocess.call(['conda', 'build', 'rdkit'])
+    subprocess.call(['git', 'clone', 'https://github.com/rdkit/conda-rdkit.git'])
+    os.chdir('conda-rdkit')
+    subprocess.call(['conda', 'build', 'boost'])
+    subprocess.call(['conda', 'build', 'rdkit'])
